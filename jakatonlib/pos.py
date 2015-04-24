5cd74817c9df8dc259e4f512fb6142bbf9371f34
Ivan Vladimir Meza Ruiz <ivanvladimir@gmail.com>
2015-04-23 20:12:33 -0500

    Change name of functin for tagging to tag

--- jakatonlib/pos.py
+++ jakatonlib/pos.py
@@ -11,19 +11,19 @@ import requests
 class POSTagger():
     """Class for POS tagging a text"""
 
-    def __init__(self, root=u"http://127.0.0.1:500"):
+    def __init__(self, root=u"http://127.0.0.1:5000"):
         self.root = root
-        self.apipos = u"/pos/api/v1.0/tag/{0}"
-        self.apipossntc = u"/pos/api/v1.0/tag/{0}/{1}"
+        self.apipos = u"/api/v1.0/tag/{0}"
+        self.apipossntc = u"/api/v1.0/tag/{0}/{1}"
 
-    def pos(self,text,lang="es"):
+    def tag(self,text,lang="es"):
         """Tags a text by calling API poswebservice and a POST request"""
         url=self.root+self.apipos.format(lang)
         res=requests.post(url=url,
                           data=text.encode('utf-8'))
         return res.json()
         
-    def pos_sntc(self,sntc,lang="es"):
+    def tag_sntc(self,sntc,lang="es"):
         """Tags a sentence by calling API poswebservice and a POST request"""
         url=self.root+self.apipossntc.format(lang,sntc)
         res=requests.get(url=url)
